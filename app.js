a291c90561c9550c39e37bef4188c0e9b1abc404
lautarotessi <lautarotessi.fotografia@gmail.com>
2022-10-30 15:33:09 -0300

    Deploy MercadoLiebre Lautaro

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+var publicDir = require('path').join(__dirname,'/public'); 
+app.use(express.static(publicDir));
+
+//RUTAS:
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.post('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/registro', (req, res) => {
+	res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+/*
+app.listen(3000, () =>
+	console.log('Servidor corriendo')
+);
+*/
+app.listen(process.env.PORT || 3000, () =>
+	console.log('Servidor corriendo')
+);
+
+
