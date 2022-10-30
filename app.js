798075cc1f54230edc598541229f962db181e1e4
lautarotessi <lautarotessi.fotografia@gmail.com>
2022-10-30 18:09:15 -0300

    cambios en app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const path = require('path');
 
@@ -14,7 +14,8 @@ app.post('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 */
-app.get('/registro', (req, res) => {
+
+/*app.get('/registro', (req, res) => {
 	res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
@@ -27,7 +28,7 @@ app.listen(3000, () =>
 	console.log('Servidor corriendo')
 );
 */
-
+/*
 app.listen(process.env.PORT || 3000, () =>
 	console.log('Servidor corriendo')
 );
@@ -35,4 +36,24 @@ app.listen(process.env.PORT || 3000, () =>
 http.listen((process.env.PORT || 5000), function(){
   console.log('listening on *:5000');
 });
+*/
+////////////////////////////////////// CÓDIGO MOISÉS
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 
+app.set('puerto',process.env.PORT || 3000)
+
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
